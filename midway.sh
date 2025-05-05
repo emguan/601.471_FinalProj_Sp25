7596efb05cc3f522d6d1e03b3790b44b9438d41e
Jarett Ren <zidar2003@gmail.com>
2025-05-05 15:04:11 -0400

    sh file

--- midway.sh
+++ midway.sh
@@ -4,12 +4,17 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=2:00:0
-#SBATCH --job-name="midway_run"
-#SBATCH --output=midway_results_128.out
+#SBATCH --job-name="sliding_test"
+#SBATCH --output=sliding.out
 #SBATCH --mem=16G
 #SBATCH --nodelist=gpuz01
 
+source /data/apps/linux-centos8-cascadelake/gcc-9.3.0/anaconda3-2020.07-i7qavhiohb2uwqs4eqjeefzx3kp5jqdu/etc/profile.d/conda.sh
+
+module load anaconda
+conda activate ssm_project
 
 module load cuda/12.1
+
 python test.py
 

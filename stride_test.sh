a4b15c3115077b26eb5cbedb11f0047b6dc8a819
cs601-jren25 <cs601-jren25@login02.cm.cluster>
2025-05-05 15:53:32 -0400

    stride test

---
+++ stride_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --partition=mig_class
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=2:00:0
+#SBATCH --job-name="stride_test"
+#SBATCH --output=stride_test.out
+#SBATCH --mem=16G
+
+source /data/apps/linux-centos8-cascadelake/gcc-9.3.0/anaconda3-2020.07-i7qavhiohb2uwqs4eqjeefzx3kp5jqdu/etc/profile.d/conda.sh
+
+module load anaconda
+conda activate ssm_project
+
+module load cuda/12.1
+
+python test.py
+
